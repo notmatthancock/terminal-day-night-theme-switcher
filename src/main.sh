ee3cec9d23468171d1e2ccb49928c961a0a4e6df
Matt Hancock <mhancock743@gmail.com>
2015-05-23 13:43:46 -0400

    -

--- src/main.sh
+++ src/main.sh
@@ -1,3 +1,3 @@
-term_theme_switch_path=/Users/macuser/scratch/terminal_day_night_theme_switcher/src
-theme=$(python $term_theme_switch_path/get_theme.py tallahasse, fl)
-osascript $term_theme_switch_path/change_theme.applescript $theme
+term_theme_switch_path=
+theme=$(python $term_theme_switch_path/get_theme.py *my location*)
+osascript $term_theme_switch_path/change_theme.applescript  $theme
